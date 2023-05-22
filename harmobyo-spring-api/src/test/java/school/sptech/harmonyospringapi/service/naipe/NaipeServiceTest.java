27f01bb3bf8686b2aae57ea4200e8ddefb1835c9
dianaaugusta <>
2023-05-22 20:20:15 -0300

    FEAT subindo incompleto testes da service de naipe

--- harmobyo-spring-api/src/test/java/school/sptech/harmonyospringapi/service/naipe/NaipeServiceTest.java
+++ harmobyo-spring-api/src/test/java/school/sptech/harmonyospringapi/service/naipe/NaipeServiceTest.java
@@ -1,6 +1,57 @@
 package school.sptech.harmonyospringapi.service.naipe;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.sptech.harmonyospringapi.domain.Instrumento;
+import school.sptech.harmonyospringapi.domain.Naipe;
+import school.sptech.harmonyospringapi.repository.NaipeRepository;
+import school.sptech.harmonyospringapi.service.exceptions.EntidadeConflitanteException;
+import school.sptech.harmonyospringapi.service.exceptions.EntitadeNaoEncontradaException;
+import school.sptech.harmonyospringapi.service.instrumento.dto.InstrumentoCriacaoDto;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
 class NaipeServiceTest {
-  
+    @Mock
+    private NaipeRepository naipeRepository;
+
+    @InjectMocks
+    private NaipeService naipeService;
+
+    @Test
+    public void quandoAcionadoComIdInvalidoDeveRetornarEntidadeNaoEncontradaException() {
+        // Arrange
+        Optional<Naipe> naipeEncontrado = naipeRepository.findById(999);
+
+        Mockito.when(naipeRepository.findById(999)).thenReturn(naipeEncontrado);
+
+        // Act & Assert
+        assertThrows(EntitadeNaoEncontradaException.class, ()
+                -> naipeService.obterNaipePorId(999));
+    }
+
+    @Test
+    public void quandoAcionadoComIdValidoDeveRetornarC() {
+        // Arrange
+        Optional<Naipe> naipeEncontrado = naipeRepository.findById(999);
+
+        Mockito.when(naipeRepository.findById(999)).thenReturn(naipeEncontrado);
+
+        // Act & Assert
+        assertThrows(EntitadeNaoEncontradaException.class, ()
+                -> naipeService.obterNaipePorId(999));
+    }
+
+
+
 }
