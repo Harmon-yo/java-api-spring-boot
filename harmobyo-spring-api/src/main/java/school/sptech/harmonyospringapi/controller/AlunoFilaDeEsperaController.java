80c67c0b2584e2db0670365b0b61cbac394444b8
dianaaugusta <joaolimah406@gmail.com>
2023-05-31 15:08:48 -0300

    FEAT adicionando documentação do swagger na controller de fila de espera

--- harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/controller/AlunoFilaDeEsperaController.java
+++ harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/controller/AlunoFilaDeEsperaController.java
@@ -1,6 +1,10 @@
 package school.sptech.harmonyospringapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +17,25 @@ import school.sptech.harmonyospringapi.utils.FilaObj;
 
 @RestController
 @RequestMapping("/fila")
+@Tag(name = "Fila de espera de Alunos, quando há uma aula para um dia já atribuído")
 public class AlunoFilaDeEsperaController {
     @Autowired
     private AlunoFilaDeEsperaService service;
 
+    @Operation( summary = "Retorna o primeiro aluno (prioridade) da fila")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Primeiro Aluno esperando na fila: ")
+    })
     @SecurityRequirement(name = "Bearer")
     @GetMapping("/primeiro-aluno")
     public ResponseEntity<AlunoFilaDeEsperaDTO> pollAlunoFilaDeEspera(){
         return ResponseEntity.ok().body(service.pollAluno());
     }
 
+    @Operation( summary = "Adiciona um aluno à fila de espera")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Alunos na fila encontrados: ")
+    })
     @SecurityRequirement(name = "Bearer")
     @PostMapping
     public ResponseEntity<FilaObj<AlunoFilaDeEsperaDTO>> postFilaAlunoDeEspera(){
