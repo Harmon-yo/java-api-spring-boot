9c46f2a457b2853aaf1ab2aa86ff895266875e44
dianaaugusta <joaolimah406@gmail.com>
2023-05-31 15:09:15 -0300

    FEAT adicionando documentação do wagger na controller de instrumento

--- harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/controller/InstrumentoController.java
+++ harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/controller/InstrumentoController.java
@@ -1,5 +1,8 @@
 package school.sptech.harmonyospringapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -16,12 +19,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/instrumentos")
-@Tag(name = "Instrumentos")
+@Tag(name = "Controller de instrumentos para os instrumentos cadastrados na API (Exemplo: Violão, Piano, Xilofone)")
 public class InstrumentoController {
 
     @Autowired
     private InstrumentoService instrumentoService;
 
+    @Operation( summary = "Cadastra um instrumento na plataforma a partir de uma DTO")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Instrumento cadastrado. "),
+            @ApiResponse(responseCode = "409", description = "Não foi possível cadastrar o instrumento. Verifique se as " +
+                    "instruções do mesmo foram cadastradas corretamente ou se o instrumento já foi cadastrado")
+    })
     @SecurityRequirement(name = "Bearer")
     @PostMapping
     public ResponseEntity<InstrumentoExibicaoDto> cadastrar(@RequestBody @Valid InstrumentoCriacaoDto instrumentoCriacaoDto) {
@@ -30,6 +39,11 @@ public class InstrumentoController {
         return instrumentoExibicaoDto == null ? ResponseEntity.status(409).build() : ResponseEntity.status(201).body(instrumentoExibicaoDto);
     }
 
+    @Operation( summary = "Lista os instrumentos cadastrados na plataforma")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista dos instrumentos cadastrados :  "),
+            @ApiResponse(responseCode = "409", description = "Não foram encontrados instrumentos. ")
+    })
     @SecurityRequirement(name = "Bearer")
     @GetMapping
     public ResponseEntity<List<InstrumentoExibicaoDto>> listar() {
