901daba52d6439202fef6c9bc59066cdcc6f9932
GabrielDuarte14 <gabriel.duarte@sptech.school>
2023-09-15 22:52:44 -0300

    [FIX] - Retirando sysOut

--- harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/service/usuario/ProfessorService.java
+++ harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/service/usuario/ProfessorService.java
@@ -376,16 +376,7 @@ public class ProfessorService {
     public Long getMediaTempoResposta(int id) {
         LocalDateTime now = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
         List<Pedido> pedidos = this.pedidoRepository.buscarPorUsuarioId(id);
-        for(Pedido p : pedidos){
-            if(p.getHoraCriacao() == null){
-                System.out.println("é isso");
-
-            }
-        }
         Long somaTempoResposta = pedidos.stream().filter(p -> p.getHoraCriacao().getMonth() == now.getMonth() && p.getHoraResposta() != null).mapToLong(p -> Duration.between(p.getHoraCriacao(), p.getHoraResposta()).toMinutes()).sum();
-        System.out.println(" =============== aqui");
-        System.out.println(somaTempoResposta);
-        System.out.println(pedidos.size());
         Long mediaTempoResposta = somaTempoResposta / pedidos.stream().filter(p -> p.getHoraResposta() != null).count();
         return mediaTempoResposta;
     }
