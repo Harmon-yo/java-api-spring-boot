11e4c93ab0bd5eda48af367967537beae1c30419
Vinicius-Alves02 <vinicius.vasques@sptech.school>
2023-10-09 18:18:21 -0300

    [NEW BRANCH] - Branch criada para armazenar apenas a versão mais recente e buildada do arquivo.jar

--- harmobyo-spring-api/src/main/java/school/sptech/harmonyospringapi/repository/InstrumentoRepository.java
+++
@@ -1,8 +0,0 @@
-package school.sptech.harmonyospringapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.harmonyospringapi.domain.Instrumento;
-
-public interface InstrumentoRepository extends JpaRepository<Instrumento, Integer> {
-    boolean existsInstrumentoByNomeIgnoreCase(String nome);
-}
